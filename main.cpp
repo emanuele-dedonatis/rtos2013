a9e9572d73d870035ab2ee383f94d5de7e448fe9
emanuele-dedonatis <emanuele.dedonatis@gmail.com>
2014-03-13 15:10:14 +0100

    LIS302 test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 
 #include <cstdio>
 #include "miosix.h"
+#include "LIS302.h"
 
 using namespace std;
 using namespace miosix;
 
+volatile int x, y, z;
+
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    LIS302::init();
+    
+    for(;;) {
+        x = LIS302::readX();
+        y = LIS302::readY();
+        z = LIS302::readZ();
+        
+        iprintf("%d %d %d \n", x, y, y);
+        
+        usleep(20000);
+    }
 }
