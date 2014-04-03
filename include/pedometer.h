566df587f0a669ae44c763406364c3a4ba81e8b4
Emanuele De Donatis <emanuele.dedonatis@gmail.com>
2014-04-03 13:18:08 +0200

    LCD design

--- include/pedometer.h
+++ include/pedometer.h
@@ -30,81 +30,99 @@ public:
    static const int MODE_RUN = 3;
    
    /**
+    * @brief Get the instance of the pedometer, since it is a singleton
+    * 
     * \return an instance of the pedometer (singleton)
     */
    static Pedometer& instance();    
    
    /**
-    * Initialize SPI and accelerometer sensor LIS302
+    * @brief Initialize SPI and accelerometer sensor LIS302
     */
    void init();
    
    /**
-    * Start the counting 
+    * @brief Start the counting 
     */
    void start();
    
    /**
-    * Pause the counting
+    * @brief Pause the counting
     */
    void pause();
    
    /**
-    * Restart the counting
+    * @brief Restart the counting
     */
    void restart();
    
    /**
+    * @brief Get the current number of steps
+    * 
     * \return current number of steps
     */
    int getSteps();
   
    /**
+    * @brief Get the current training mode (STEADY or WALK or RUN)
+    * 
     * \return the current training mode 
     */
    int getMode();
    
    /**
+    * @brief Ask if is in steady mode
+    * 
     * \return true if is steady mode 
     */
    bool isSteady();
     
    /**
+    * @brief Ask if is in walk mode
+    * 
     * \return true if is walk mode 
     */   
    bool isWalk();
     
    /**
+    * @brief Ask if is in run mode
+    * 
     * \return true if is run mode 
     */   
    bool isRun();
    
    /**
-    * Set the user's height
+    * @brief Set the user's height
     * 
     * \param m Height in meters
     */
    void setHeight(float m);
    
    /**
-    * Set the user's weight
+    * @brief Set the user's weight
     * 
     * \param kg Weight in kilos
     */
    void setWeight(float kg);
    
    /**
+    * @brief Get the current distance in km
+    * 
     * \return current distance [km]
     */
    float getDistance();
    
    /**
+    * @brief Get the current speed in km/sec
+    * 
     * \return current speed [km/sec]
     */
    float getSpeed();
    
    /**
-    * \return current calories burned 
+    * @brief Get the current calories burned in KCal
+    * 
+    * \return current calories burned [Kcal]
     */
    float getCalories();
    
--- main.cpp
+++ main.cpp
@@ -28,39 +28,66 @@ typedef Gpio<GPIOE_BASE,12>  d7;        //pin14
 
 typedef Gpio<GPIOA_BASE,0>  button;        //board button
 
-#define LCD_ROW         2
-#define LCD_COL         8
+#define LCD_ROW         4
+#define LCD_COL         20
+
+Lcd44780 lcd(rs::getPin(), e::getPin(), d4::getPin(), d5::getPin(), d6::getPin(), d7::getPin(), LCD_ROW, LCD_COL);
 
 void pedometerTask(void *argv) {
     Pedometer::instance().init();
     Pedometer::instance().start();
 }
 
+void initLcd() {
+    lcd.clear();
+    lcd.go(0,0);
+    lcd.printf("S:");
+    lcd.go(8,0);
+    lcd.printf("M:");
+    lcd.go(0,1);
+    lcd.printf("V:");
+    lcd.go(7,1);
+    lcd.printf("K:");
+    lcd.go(13,1);
+    lcd.printf("C:");
+}
+
 int main()
 {
-    Lcd44780 lcd(rs::getPin(), e::getPin(), d4::getPin(), d5::getPin(), d6::getPin(), d7::getPin(), LCD_ROW, LCD_COL);
+    initLcd();
     Thread *pedometer_t;
     pedometer_t = Thread::create(pedometerTask, 2048, 1, NULL, Thread::JOINABLE);
-    float calories = 0;
+    
+    Pedometer& pedo = Pedometer::instance();
     for(;;){
-        calories = Pedometer::instance().getCalories();
-        lcd.clear();
-        lcd.go(0,0);
-        lcd.printf("%.2f", calories);
-        lcd.go(0,1);
-        lcd.printf("%d ", Pedometer::instance().getSteps());
+        //STEPS
+        lcd.go(3,0);
+        lcd.printf("%d", pedo.getSteps());
+        //MODE
+        lcd.go(10,0);
         switch(Pedometer::instance().getMode()) {
             case Pedometer::MODE_STEADY:
                 lcd.printf("STEADY");
                 break;
             case Pedometer::MODE_WALK:
-                lcd.printf("WALK");
+                lcd.printf(" WALK ");
                 break;            
             case Pedometer::MODE_RUN:
-                lcd.printf("RUN");
+                lcd.printf(" RUN  ");
                 break; 
-            default: lcd.printf("");
+            default: lcd.printf("      ");
         }
+        //SPEED
+        lcd.go(2,1);
+        lcd.printf("    ");
+        lcd.go(2,1);
+        lcd.printf("%.1f", pedo.getSpeed());
+        //DISTANCE
+        lcd.go(9,1);
+        lcd.printf("%.1f", pedo.getDistance());
+        //CALORIES
+        lcd.go(15,1);
+        lcd.printf("%.1f", pedo.getCalories());
         if(button::value())
              Pedometer::instance().restart();
         usleep(50000);
